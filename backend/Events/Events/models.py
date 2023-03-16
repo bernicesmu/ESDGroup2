cf7e46b6d1df4f5361ad9c651ab2a08b70e2c19f
regineshalom <reginetan.2021@scis.smu.edu.sg>
2023-03-17 04:21:13 +0800

    i hope that this takes away the 800k commits if not god so help me i am confused

--- backend/Events/Events/models.py
+++ backend/Events/Events/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
-class event(models.Model):
-    clubName=models.CharField(max_length=255),
-    eventName=models.CharField(max_length=255),
-    eventConfirmed=True, 
-    eventType=models.CharField(max_length=255),
-    eventLocation=models.CharField(max_length=255),
+# Create your models here.
+"""
+    clubName: "SMUBIA",
+    eventName: "",
+    eventConfirmed: true, 
+    eventType: "",
+    eventLocation: "",
     eventDate: "01-01-23",
     eventFromTime: "x", 
     eventToTime: "x", 
     eventSignUpForm: "",
+"""
+class Event(models.Model):
+    clubName=models.CharField(max_length=255)
+    eventName=models.CharField(max_length=255)
+    eventConfirmed=models.BooleanField()
+    eventType=models.CharField(max_length=255)
+    eventLocation=models.CharField(max_length=255)
+    eventDate=models.DateField()
+    eventFromTime=models.DateTimeField()
+    eventToTime=models.DateTimeField()
+    eventSignUpForm=models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.eventName
