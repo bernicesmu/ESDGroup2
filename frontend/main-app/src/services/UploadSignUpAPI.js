b68a59e0294e215c11b28d0c1d8cddc0cb86dffc
bernicesmu <bernice.teo.2021@SCIS.SMU.EDU.sg>
2023-03-28 17:31:21 +0800

    added more APIs for upload sign up

--- frontend/main-app/src/services/UploadSignUpAPI.js
+++ frontend/main-app/src/services/UploadSignUpAPI.js
@@ -15,3 +15,25 @@ export async function uploadSignUps(signUpFile) {
         return error; 
     }
 }
+
+export async function getSignUpByEventId(eventId) { 
+    let api_url = 'http://localhost:5108/getSignUpDetails/' + eventId; 
+    try { 
+        const response = await axios.get(api_url); 
+        console.log('response ', response); 
+        return response.data; 
+    } catch(error) { 
+        return error; 
+    }
+}
+
+export async function broadcastMessage(data) { 
+    let api_url = 'http://localhost:5108/broadcast' 
+    try { 
+        const response = await axios.post(api_url, data); 
+        console.log('response ', response); 
+        return response.data; 
+    } catch(error) { 
+        return error; 
+    }
+}
