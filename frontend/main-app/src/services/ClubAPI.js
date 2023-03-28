3c5f333bd2876ffa3581616bec59727f28e66d77
bernicesmu <bernice.teo.2021@SCIS.SMU.EDU.sg>
2023-03-28 17:23:04 +0800

    added the APIs on the frontend

--- frontend/main-app/src/services/ClubAPI.js
+++ frontend/main-app/src/services/ClubAPI.js
@@ -1,1 +1,23 @@
 import axios from 'axios';
+
+export async function getAllClubs() { 
+    let api_url = 'http://localhost:5102/api/clubs'; 
+    try { 
+        const response = await axios.get(api_url); 
+        console.log('response ', response); 
+        return response.data; 
+    } catch(error) { 
+        return error; 
+    }
+}
+
+export async function createClub(club) { 
+    let api_url = 'http://localhost:5102/api/clubs'; 
+    try { 
+        const response = await axios.post(api_url, club);
+        console.log('response ', response); 
+        return response.data; 
+    } catch(error) { 
+        return error; 
+    }
+}
--- frontend/main-app/src/services/EventAPI.js
+++ frontend/main-app/src/services/EventAPI.js
@@ -1,1 +1,34 @@
 import axios from 'axios';
+
+export async function createEvent(event) { 
+    let api_url = 'http://localhost:5101/eventList';
+    try { 
+        const response = await axios.post(api_url, event); 
+        console.log('response ', response); 
+        return response.data;
+    } catch(error) { 
+        return error; 
+    }
+}
+
+export async function getAllEvents() { 
+    let api_url = 'http://localhost:5101/allEvents';
+    try { 
+        const response = await axios.get(api_url); 
+        console.log('response ', response); 
+        return response.data; 
+    } catch(error) { 
+        return error; 
+    }
+}
+
+export async function getEventById(eventId) { 
+    let api_url = 'http://localhost:5101/event/' + eventId; 
+    try { 
+        const response = await axios.get(api_url); 
+        console.log('response ', response); 
+        return response.data; 
+    } catch(error) { 
+        return error; 
+    }
+}
