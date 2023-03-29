f08709e91401a81f027bf26377ff941f7bceddd6
regineshalom <reginetan.2021@scis.smu.edu.sg>
2023-03-30 01:24:15 +0800

    modified the column name

--- backend/Events/api/settings.py
+++ backend/Events/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2!!8wxsfz+eox%2d77%a)25-q7!uk_#oln-ugv$-m*!s4wxax^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -127,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH=False
