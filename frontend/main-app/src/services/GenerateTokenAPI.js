cbe55d026aa852886c9274ddedcc46bf436eceb5
bryanleezh <leebryan307@gmail.com>
2023-03-31 16:08:50 +0800

    added generatetokenapi

--- frontend/main-app/src/services/GenerateTokenAPI.js
+++ frontend/main-app/src/services/GenerateTokenAPI.js
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+export async function generateToken(data) {
+    let api_url = 'http://localhost:5109/getToken';
+    try {
+        const response = await axios.post(api_url, data);
+        console.log('response  ', response);
+        return response.data;
+    } catch(error) {
+        return error; 
+    }
+}
+
+export function decodeToken(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = decodeURIComponent(atob(base64Url).split('').map((c) => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(base64);
+}
