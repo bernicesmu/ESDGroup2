e151d52d897323e88d9a4f6f27c1fc01ebc1af89
boopyschloopy <atrayeed.2021@scis.smu.edu.sg>
2023-04-03 23:45:35 +0800

    update sql script w data
    idk what happ to the docker-compose

--- backend/attendance/attendance-db/attendance_db.sql
+++ backend/attendance/attendance-db/attendance_db.sql
@@ -3,16 +3,34 @@ USE `attendancesqldb`;
 
 DROP TABLE IF EXISTS `sign_ups`;
 CREATE TABLE IF NOT EXISTS `sign_ups` (
-  `id` SERIAL,
   `eventId` varchar(20) NOT NULL,
   `studentMatricNum` varchar(20) NOT NULL,
-  `signUp` INTEGER,
-  PRIMARY KEY (`id`)
+  `signUp` INT,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `sign_ups` (`id`, `eventId`, `studentMatricNum`, `signUp`) VALUES
-('1', '1','97811294',1),
-('2', '23', '97813494',1),
-('3', '34', '34474234', 1),
-('4', '43', '32146785', 1),
-('5', '4', '12345678', 1);
+INSERT INTO `sign_ups` (`eventId`, `studentMatricNum`, `signUp`) VALUES
+('1','1429384',1),
+('1','1338329',1),
+('1','1301938',1),
+('1','1237819',1),
+('1','1815063',1),
+('1','1429605',1),
+('1','1760382',1),
+('2','1338329',1),
+('2','1301938',1),
+('3','1338329',1),
+('3','1301938',1),
+('3','1338329',1),
+('3','1301938',1),
+('3','1302934',1),
+('4','1338329',1),
+('4','1301938',1),
+('4','1419345',1),
+('4','1986754',1),
+('4','1237819',1),
+('5','1302934',1),
+('6','1301938',1),
+('6','1338329',1),
+('6','1301938',1),
+('6','1429605',1),
+('6','1760382',1);
