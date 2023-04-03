f4c992ecf0d021d559b2c5dc41a4ca3f12b6ad63
Ivan Yeo <ivan.yeowq@gmail.com>
2023-04-03 16:30:33 +0800

    Added ClubExcos table seeder

--- backend/club_zipped/database/seeders/DatabaseSeeder.php
+++ backend/club_zipped/database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(ClubsTableSeeder::class);
         $this->call(ClubMembersTableSeeder::class);
+        $this->call(ClubExcosTableSeeder::class);
     }
 }
