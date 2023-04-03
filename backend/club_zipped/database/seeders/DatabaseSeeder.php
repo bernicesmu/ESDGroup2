b7e7ff9d13f63774a4ad016b0dfce3e34e9eb57d
Ivan Yeo <ivan.yeowq@gmail.com>
2023-04-03 16:13:31 +0800

    Added ClubMembers table seeder

--- backend/club_zipped/database/seeders/DatabaseSeeder.php
+++ backend/club_zipped/database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(ClubsTableSeeder::class);
+        $this->call(ClubMembersTableSeeder::class);
     }
 }
